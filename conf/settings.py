ee43e528403ada27d81fdb1c43b1b0a6d7e7424e
beahoop <beahoopdesign@gmail.com>
2021-03-08 11:31:55 -0500

    updated settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['127.0.0.1', 'django-batch-maker-beahoop.herokuapp.com/']
-
-
+ALLOWED_HOSTS =  ['127.0.0.1', 'django-batch-maker-beahoop.herokuapp.com']
 
 # Application definition
 
